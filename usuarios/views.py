764e126951461c2c4c4fa7b43ffb62ea52637c35
Andriel Alexandre <an-driel@hotmail.com>
2023-01-15 22:21:33 -0300

    second

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.messages import constants
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
+
 
 def cadastro(request):
     if request.method == 'GET':
@@ -32,4 +35,27 @@ def cadastro(request):
         except:
             messages.add_message(request, constants.ERROR, 'Erro interno do sistema')
             return render(request, 'cadastro.html')
-        
+
+
+def logar(request):
+    if request.user.is_authenticated:
+        return redirect('/divulgar/novo_pet')
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        nome = request.POST.get('nome')
+        senha = request.POST.get('senha')
+        user = authenticate(username=nome,
+                            password=senha)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/divulgar/novo_pet')
+        else:
+            messages.add_message(request, constants.ERROR, 'Usuário ou senha incorretos')
+            return render(request, 'login.html')
+
+
+def sair(request):
+    logout(request)
+    return redirect('/auth/login')
